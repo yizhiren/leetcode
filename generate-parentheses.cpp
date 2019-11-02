0bb1523d51c890fceff832f5722b988c4b8549cc
yizhiren <jin-man@qq.com>
2019-11-03 00:53:53 +0800

    Update generate-parentheses.cpp

--- generate-parentheses.cpp
+++ generate-parentheses.cpp
@@ -25,3 +25,34 @@ public:
         return result;
     }
 };
+//////////////////////
+class Solution {
+public:
+
+    vector<string> generateParenthesis(int n) {
+        if(n==0){
+            return vector<string>();
+        }
+        if(n==1){
+            return vector<string>{"()"};
+        }
+        
+        const string pr = "()";
+        
+        set<string> pre_level;
+        pre_level.insert("()");
+        
+        for(int i=1;i<n;i++){
+            set<string> level;
+            for(auto &str: pre_level){
+                int strlen = str.length();
+                for(int j=0;j<strlen;j++){
+                    level.insert(str.substr(0,j)+pr+str.substr(j));
+                }
+                level.insert(str + pr);
+            }
+            pre_level.swap(level);
+        }
+        return vector<string>(pre_level.begin(),pre_level.end());
+    }
+};
