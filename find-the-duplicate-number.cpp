49b58fe11769589217534630bf15fccb25619305
yizhiren <jin-man@qq.com>
2019-12-15 23:32:18 +0800

    Update find-the-duplicate-number.cpp

--- find-the-duplicate-number.cpp
+++ find-the-duplicate-number.cpp
@@ -35,3 +35,31 @@ public:
         return -1;
     }
 };
+
+////
+// https://leetcode.com/problems/find-the-duplicate-number/solution/ 
+// solution 3 快慢指针判断环的位置
+class Solution {
+public:
+
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast=0;
+        do{
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+            if(slow==fast){
+                break;
+            }
+        }while(1);
+        int slow2=0;
+        while(slow2!=slow){
+            slow=nums[slow];
+            slow2=nums[slow2];
+        }
+        return slow;
+        
+    }
+};
+
