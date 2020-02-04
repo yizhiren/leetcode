cc869f0c0353d406523774fffa8fbb24534a62be
yizhiren <jin-man@qq.com>
2020-02-05 00:09:35 +0800

    Update perfect_squares.cpp

--- perfect_squares.cpp
+++ perfect_squares.cpp
@@ -1,6 +1,24 @@
 
+/// DP
+class Solution {
+public:
+   
+    int numSquares(int n) {
+        if(n<=0)return 0;
+        
+        vector<int> dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            dp[i]=i;
+            for(int j=0;i-j*j>=0;j++){
+                dp[i]=min(dp[i],1+dp[i-j*j]);
+            }
+        }
+        
+        return dp[n];
+    }
+};
 
-
+//////// dfs+cache
 
 class Solution {
 
